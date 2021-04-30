518ba14413336bebbdef640be8816d4dc6b574e4
Zhiguang Liu <zhiguang.liu@intel.com>
2021-04-30 14:17:56 +0800

    MdeModulePkg/ACPI: Install ACPI table from HOB.
    If HOB contains APCI table information, entry point of AcpiTableDxe.inf
    should parse the APCI table from HOB, and install these tables.
    We assume the whole ACPI table (starting with EFI_ACPI_2_0_ROOT_SYSTEM_DESCRIPTION_POINTER)
    is contained by a single gEfiAcpiTableGuid HOB.
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Dandan Bi <dandan.bi@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Ray Ni <ray.ni@intel.com>
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

--- MdePkg/Include/UniversalPayload/AcpiTableHob.h
+++ MdePkg/Include/UniversalPayload/AcpiTableHob.h
@@ -10,14 +10,16 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define _ACPI_TABLE_HOB_H_
 
 #include <Uefi.h>
+#include <UniversalPayload/PldPciRootBridgeInfoHob.h>
 
 #pragma pack(1)
 typedef struct {
-  EFI_PHYSICAL_ADDRESS          Rsdp;
+  PLD_GENERIC_HEADER        PldHeader;
+  EFI_PHYSICAL_ADDRESS      Rsdp;
 } PLD_ACPI_TABLE_HOB;
 
 #pragma pack()
 
 extern GUID gPldAcpiTableGuid;
 
-#endif
+#endif //_ACPI_TABLE_HOB_H_
