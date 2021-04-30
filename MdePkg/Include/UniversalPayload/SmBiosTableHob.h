8a5abfd42d8b9c056c3db065bb4fe11cf966cb10
Zhiguang Liu <zhiguang.liu@intel.com>
2021-04-30 13:35:04 +0800

    MdePkg: Add new structure for the Universal Payload SMBios Table Info Hob
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

---
+++ MdePkg/Include/UniversalPayload/SmBiosTableHob.h
@@ -0,0 +1,26 @@
+/** @file
+ Define the GUID gPldSmbios3TableGuid and gPldSmbiosTableGuid HOB struct.
+
+Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _SMBIOS_TABLE_HOB_H_
+#define _SMBIOS_TABLE_HOB_H_
+
+#include <Uefi.h>
+#include <UniversalPayload/PldPciRootBridgeInfoHob.h>
+
+#pragma pack(1)
+
+typedef struct {
+  PLD_GENERIC_HEADER     PldHeader;
+  EFI_PHYSICAL_ADDRESS   SmBiosEntryPoint;
+} PLD_SMBIOS_TABLE_HOB;
+
+#pragma pack()
+
+extern GUID gPldSmbios3TableGuid;
+extern GUID gPldSmbiosTableGuid;
+#endif
