3ae483a3bcda57f9a8dd7a8846707f6f8c243d0e
Zhiguang Liu <zhiguang.liu@intel.com>
2021-11-01 21:03:27 +0800

    MdeModulePkg: Define Guid Hob for Memory Map table
    Define Guid Hob for Memory Map table.
    
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

---
+++ MdeModulePkg/Include/UniversalPayload/MemoryMap.h
@@ -0,0 +1,35 @@
+/** @file
+ Define the structure for the Universal Payload Memory map.
+
+Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Revision Reference:
+    - Universal Payload Specification 0.75 (https://universalpayload.github.io/documentation/)
+**/
+
+#ifndef UNIVERSAL_PAYLOAD_MEMORY_MAP_H_
+#define UNIVERSAL_PAYLOAD_MEMORY_MAP_H_
+
+#include <Uefi.h>
+#include <UniversalPayload/UniversalPayload.h>
+
+#define EFI_MEMORY_PRESENT      0x0100000000000000ULL
+#define EFI_MEMORY_INITIALIZED  0x0200000000000000ULL
+#define EFI_MEMORY_TESTED       0x0400000000000000ULL
+
+#pragma pack (1)
+
+typedef struct {
+  UNIVERSAL_PAYLOAD_GENERIC_HEADER   Header;
+  UINT16                             Count;
+  UINT16                             DescriptorSize;
+  EFI_MEMORY_DESCRIPTOR              MemoryMap[0];
+} UNIVERSAL_PAYLOAD_MEMORY_MAP;
+
+#pragma pack()
+
+#define UNIVERSAL_PAYLOAD_MEMORY_MAP_REVISION 1
+
+extern GUID gUniversalPayloadMemoryMapGuid;
+#endif
