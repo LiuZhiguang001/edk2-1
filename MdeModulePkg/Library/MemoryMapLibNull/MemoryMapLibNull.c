67ea01928aa066578070d47c4f5e4fb102565c6f
Zhiguang Liu <zhiguang.liu@intel.com>
2022-02-24 13:06:54 +0800

    MdeModulePkg: Add MemoryMapLibNull library
    For platforms which doesn't need to transfer some Hobs into memory map format,
    use this null library.
    
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

---
+++ MdeModulePkg/Library/MemoryMapLibNull/MemoryMapLibNull.c
@@ -0,0 +1,19 @@
+/** @file
+
+  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+#include <Uefi.h>
+
+EFI_STATUS
+EFIAPI
+BuildMemMap (
+  VOID
+  )
+{
+  return EFI_SUCCESS;
+}
